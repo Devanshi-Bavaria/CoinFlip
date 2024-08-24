4449a50a4dbecad8fb963f1fae19c0e47406c0b6
Devanshi Bavaria <bavariadevanshi@gmail.com>
2024-08-24 10:08:21 +0530

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -30,7 +30,7 @@ const space = {
 
 const fonts = {
   body: "'Lora', serif", // Elegant, classic serif font
-  heading: "'Playfair Display', serif", // Stylish and elegant for headings
+  heading: heading: "'Merriweather', serif", // Stylish and elegant for headings
   logo: "'Great Vibes', cursive", // Elegant cursive for the logo
 };
 
